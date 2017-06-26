8a12a1d3dc4958434b1018879211f0e9e325ea22
Elliot Smith <elliot.smith@bbc.co.uk>
2017-06-26 12:25:42 +0100

    Add tests for LOD->resolve() and LOD->setPrefix()

--- tests/unit/LODTest.php
+++ tests/unit/LODTest.php
@@ -19,6 +19,7 @@
 
 /* Unit tests for LOD */
 use res\liblod\LOD;
+use res\liblod\LODResponse;
 
 use PHPUnit\Framework\TestCase;
 
@@ -26,21 +27,88 @@ const TURTLE = <<<TURTLE
 @prefix dcterms: <http://purl.org/dc/terms/> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
 @prefix schema: <http://schema.org/> .
+@prefix foo: <http://foo.bar/> .
 
 <http://foo.bar/something>
   dcterms:title "Bar" ;
   rdfs:label "Foo" ;
-  schema:name "Baz" .
+  schema:name "Baz" ;
+  foo:appelation "Boo" .
 TURTLE;
+
+final class FakeHttpClient
+{
+    private $responseMap;
+
+    // $response: either a canned LODResponse object returned for all requests,
+    // or an associative array mapping from URIs to LODResponse objects;
+    // if a single response, this is mapped to the special URI pattern '*',
+    // meaning this response is returned for all request URIs
+    public function __construct($response)
+    {
+        if($response instanceof LODResponse)
+        {
+            $this->responseMap = array(
+                '*' => $response
+            );
+        }
+        else
+        {
+            $this->responseMap = $response;
+        }
+    }
+
+    public function get($uri)
+    {
+        if(array_key_exists('*', $this->responseMap))
+        {
+            return $this->responseMap['*'];
+        }
+        else if(array_key_exists($uri, $this->responseMap))
+        {
+            return $this->responseMap[$uri];
+        }
+        else
+        {
+            trigger_error("no fake response available for $uri");
+        }
+    }
+}
 
 final class LODTest extends TestCase
 {
+    function testSetPrefix()
+    {
+        $lod = new LOD();
+        $lod->setPrefix('bar', 'http://foo.bar/');
+
+        $lod->loadRdf(TURTLE, 'text/turtle');
+        $lodinstance = $lod['http://foo.bar/something'];
+
+        $expected = 'Boo';
+        $actual = "{$lodinstance['bar:appelation']}";
+        $this->assertEquals($expected, $actual);
+    }
+
+    function testResolve()
+    {
+        $fakeResponse = new LODResponse();
+        $fakeResponse->payload = TURTLE;
+        $fakeResponse->type = 'text/turtle';
+
+        $fakeClient = new FakeHttpClient($fakeResponse);
+
+        $lod = new LOD($fakeClient);
+        $lodinstance = $lod->resolve('http://foo.bar/something');
+
+        $this->assertEquals(4, count($lodinstance->model));
+    }
+
     function testLoadRdfTurtle()
     {
         $lod = new LOD();
         $lod->loadRdf(TURTLE, 'text/turtle');
         $lodinstance = $lod->resolve('http://foo.bar/something');
-        $this->assertEquals(3, count($lodinstance->model));
+        $this->assertEquals(4, count($lodinstance->model));
     }
 }
-?>
